af94b2d7767fd96f8defdb480d3cc1aaf369d692
Krishna Bhunia <bhunia.k@pg.com>
2024-09-22 22:52:29 +0530

    intermediate

--- equalexperts_dataeng_exercise/ingest.py
+++ equalexperts_dataeng_exercise/ingest.py
@@ -1,8 +1,17 @@
 import json
 import sys
+import duckdb
 
 # The following code is purely illustrative
 try:
+    print(sys.argv[1])
+    data = duckdb.read_json(sys.argv[1])
+    print(data)
+    print(type(data))
+    q = f"SELECT * FROM {sys.argv[1]}"
+    print(q)
+    new_data = duckdb.sql(q)
+    print(new_data)
     with open(sys.argv[1]) as votes_in:
         for line in votes_in:
             print(json.loads(line))
