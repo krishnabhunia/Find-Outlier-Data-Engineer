871ec1814a0862cd0ad5a9ad935b16bfbbfc3802
Krishna Bhunia <bhunia.k@pg.com>
2024-09-23 17:23:25 +0530

    sql solution

--- equalexperts_dataeng_exercise/outliers.py
+++ equalexperts_dataeng_exercise/outliers.py
@@ -1,79 +1,65 @@
 import duckdb
-from datetime import datetime
-from collections import defaultdict
-import db
 
-db.run_main_db()
+# db.run_main_db()
 
 DB_NAME = "warehouse.db"
 DB_SCHEMA_NAME = "blog_analysis"
 DB_TABLE_NAME = "votes"
+VIEW_NAME = "outlier_weeks"
+FULL_VIEW_NAME = F"{DB_SCHEMA_NAME}.{VIEW_NAME}"
 DB_TABLE_FULL_NAME = f"{DB_SCHEMA_NAME}.{DB_TABLE_NAME}"
 FILE_NAME = "uncommitted/votes_test.jsonl"
 
-try:
-    conn = duckdb.connect("warehouse.db")
-    cursor = conn.cursor()
-    sql_query = f"SELECT * FROM {DB_TABLE_FULL_NAME}"
-    data_rows = cursor.execute(sql_query).fetchall()
+# List of datetime objects (You can replace this with your actual list)
+conn = duckdb.connect("warehouse.db")
+cursor = conn.cursor()
 
-    # Print the fetched rows
-    # for row in data_rows:
-    #     date_str = str(row[3])
-    #     print(row, date_str)
-    #     date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-    #     week_number = date_obj.isocalendar()[1]
-        # if week_number >= 52:
-        #     week_number = week_number - 52
-        # print(f"Week number: {week_number}, and data_obj {date_obj.isocalendar()}")
+sql_query = f"DROP VIEW IF EXISTS {FULL_VIEW_NAME}; \
+CREATE VIEW {FULL_VIEW_NAME} AS \
+WITH yearly_vote_count AS ( \
+SELECT  \
+    EXTRACT(YEAR FROM CreationDate) AS year,\
+    COUNT(DISTINCT EXTRACT(WEEK FROM CreationDate)) AS total_weeks,\
+    COUNT(Id) AS total_vote_count, \
+FROM {DB_TABLE_FULL_NAME}\
+    GROUP BY year \
+    ), \
+weekly_vote_count AS ( \
+    SELECT \
+        EXTRACT(YEAR FROM CreationDate) AS year, \
+        CASE \
+            WHEN EXTRACT(WEEK FROM CreationDate) = 52 THEN 0 \
+            ELSE EXTRACT(WEEK FROM CreationDate) \
+        END AS week_number,\
+        COUNT(*) AS weekly_vote_count \
+    FROM {DB_TABLE_FULL_NAME} \
+    GROUP BY year, week_number \
+), \
+outlier_cal AS ( \
+select w.year, w.week_number, w.weekly_vote_count,  \
+    ABS(1 - (w.weekly_vote_count / (y.total_vote_count / y.total_weeks))) AS weekly_vote_ratio, \
+    CASE \
+        WHEN weekly_vote_ratio > 0.2 then TRUE \
+        ELSE FALSE \
+    END as filter_param \
+        from weekly_vote_count w join yearly_vote_count y on w.year = y.year )\
+SELECT * FROM outlier_cal \
+WHERE filter_param = TRUE \
+"
+summary = cursor.execute(sql_query).fetchall()
 
-    # new code
-    week_count = defaultdict(int)
+view_query = f"select Year as Year, week_number as WeekNumber, weekly_vote_count as VoteCount from {FULL_VIEW_NAME}"
 
-    # Iterate over the list of date strings
-    for row in data_rows:
-        # Extract the year and week number
-        date_obj = row[3]
-        # print(f"date object {date_obj}, and iso {date_obj.isocalendar()}")
-        year, week_number, _ = date_obj.isocalendar()
+outlier_table = cursor.execute(view_query).fetchall()
 
-        if year != date_obj.year:
-            print(f"Year mismatch: {year} != {date_obj.year}")
-            year = date_obj.year
-            print(f"identified week:{week_number}")
-            if week_number >= 52:
-                week_number = 0
-            elif week_number == 0:
-                week_number = 52
+print("summary")
+for r in summary:
+    print("summary:", r)
 
-            print(f"Final data after conversion {year, week_number}")
-        
-        # Increment the count for the (year, week_number) combination
-        week_count[(year, week_number)] += 1
+column_names = [desc[0] for desc in cursor.description]
 
-    # Convert the dictionary to a list of tuples with (year, week_number, count)
-    week_list = [(year, week_number, count) for (year, week_number), count in week_count.items()]
+# Print column names
+print(column_names)
 
-    year_summary = defaultdict(lambda: {'total_weeks': 0, 'sum_votes': 0})
-
-    # Aggregate data per year
-    for (year, week_number), count in week_count.items():
-        year_summary[year]['total_weeks'] += 1  # Count unique weeks
-        year_summary[year]['sum_votes'] += count  # Sum of occurrences
-
-    # Print the results for each year
-    for year, summary in year_summary.items():
-        print(f"Year: {year}, Total Weeks: {summary['total_weeks']}, Sum of Counts: {summary['sum_votes']}")
-
-    # Print the result
-    # print(week_list)
-    for w in week_list:
-        w.append("hello")
-        print(w)
-    print(year_summary)
-    # print(week_list)
-
-except Exception as ex:
-    print(f"Error : {ex}")
-finally:
-    conn.close()
+for r in outlier_table:
+    print(r)
