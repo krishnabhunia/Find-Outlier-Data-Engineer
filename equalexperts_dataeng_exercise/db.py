a27d678380848a075569d4b96973f3b849e18e18
Krishna Bhunia <bhunia.k@pg.com>
2024-09-24 17:14:42 +0530

    rearrangement

--- equalexperts_dataeng_exercise/db.py
+++ equalexperts_dataeng_exercise/db.py
@@ -6,67 +6,17 @@ import equalexperts_dataeng_exercise.config as cfg
 
 DB_NAME = cfg.DB_NAME
 DB_SCHEMA_NAME = cfg.DB_SCHEMA_NAME
-DB_TABLE_NAME = cfg.DB_TABLE_NAME
-DB_TABLE_FULL_NAME = f"{DB_SCHEMA_NAME}.{DB_TABLE_NAME}"
 
 
-def create_database():
+def create_database_and_schema():
     try:
         # Connect to the database
         conn = duckdb.connect(DB_NAME)
-        print(f"Connected to the database: {DB_NAME}")
+        print(f"Created and connected to the database: '{DB_NAME}'")
 
         # Create schema 'blog_analysis' if it doesn't exist
         conn.execute(f"CREATE SCHEMA IF NOT EXISTS {DB_SCHEMA_NAME}")
-        print(f"Created schema :{DB_SCHEMA_NAME}")
-
-        # Create a new table in the database
-        conn.execute(f'''
-            CREATE TABLE IF NOT EXISTS {DB_TABLE_FULL_NAME}(
-                Id INTEGER PRIMARY KEY,
-                PostId INTEGER,
-                VoteTypeId INTEGER,
-                CreationDate TIMESTAMP
-            );
-        ''')
-        print(f"Table created successfully:{DB_TABLE_FULL_NAME}")
-    except Exception as ex:
-        print(f"Error : {ex}")
-    finally:
-        # Close the connection
-        conn.close()
-
-
-def insert_data_into_database(file_name):
-    try:
-        print(f"Inserting data from file: {file_name}")
-        insert = f"""INSERT INTO {DB_TABLE_FULL_NAME}
-            SELECT Distinct(Id), PostId, VoteTypeId, CreationDate FROM '{file_name}'"""
-
-        # print("Insert query:", insert)
-
-        conn = duckdb.connect("warehouse.db")
-        res = conn.execute(insert).fetchall()
-
-        print(
-            f"Data inserted successfully:{DB_TABLE_FULL_NAME} and number of rows:{res[0][0]}")
-    except Exception as ex:
-        print(f"Error : {ex}")
-    finally:
-        # Close the connection
-        conn.close()
-
-
-def display_data(rows=3):
-    try:
-        conn = duckdb.connect("warehouse.db")
-        cursor = conn.cursor()
-        sql_query = f"SELECT * FROM {DB_TABLE_FULL_NAME} ORDER BY Id DESC LIMIT {rows}"
-        data_rows = cursor.execute(sql_query).fetchall()
-
-        # Print the fetched rows
-        for row in data_rows:
-            print(row)
+        print(f"Created schema :'{DB_SCHEMA_NAME}' under Database : '{DB_NAME}'")
     except Exception as ex:
         print(f"Error : {ex}")
     finally:
@@ -75,19 +25,19 @@ def display_data(rows=3):
 
 
 def remove_database():
-    db_path = DB_NAME
-    if os.path.exists(db_path):
-        os.remove(db_path)
-        print(f"Database '{db_path}' deleted successfully.")
-    else:
-        print(f"Database '{db_path}' does not exist.")
+    try:
+        if os.path.exists(DB_NAME):
+            os.remove(DB_NAME)
+            print(f"Database '{DB_NAME}' deleted successfully.")
+        else:
+            print(f"Database '{DB_NAME}' does not exist.")
+    except Exception as ex:
+        print(f"Error : {ex}")
 
 
 def run_main_db():
     remove_database()
-    create_database()
-    insert_data_into_database()
-    display_data(3)
+    create_database_and_schema()
 
 
 if __name__ == "__main__":
--- equalexperts_dataeng_exercise/ingest.py
+++ equalexperts_dataeng_exercise/ingest.py
@@ -1,18 +1,81 @@
 import sys
-
+import duckdb
 import equalexperts_dataeng_exercise.db as db
+import equalexperts_dataeng_exercise.config as cfg
+
+
+DB_NAME = cfg.DB_NAME
+DB_SCHEMA_NAME = cfg.DB_SCHEMA_NAME
+DB_TABLE_FULL_NAME = cfg.DB_TABLE_FULL_NAME
+
+
+def create_table():
+    try:
+        # Connect to the database and create table
+        conn = duckdb.connect(DB_NAME)
+        conn.execute(f'''
+            CREATE TABLE IF NOT EXISTS {DB_TABLE_FULL_NAME}(
+                Id INTEGER PRIMARY KEY,
+                PostId INTEGER,
+                VoteTypeId INTEGER,
+                CreationDate TIMESTAMP
+            );
+        ''')
+        print(f"Table created successfully : '{DB_TABLE_FULL_NAME}'")
+    except Exception as ex:
+        print(f"Error : {ex}")
+    finally:
+        conn.close()
+
+
+def insert_data_into_database(file_name):
+    try:
+        print(f"Inserting data from file: {file_name}")
+        insert = f"""INSERT INTO {DB_TABLE_FULL_NAME}
+            SELECT Distinct(Id), PostId, VoteTypeId, CreationDate FROM '{file_name}'"""
+
+        # print("Insert query:", insert)
+
+        conn = duckdb.connect(DB_NAME)
+        res = conn.execute(insert).fetchall()
+
+        print(
+            f"Data inserted successfully:'{DB_TABLE_FULL_NAME}' and number of rows:{res[0][0]}")
+    except Exception as ex:
+        print(f"Error : {ex}")
+    finally:
+        # Close the connection
+        conn.close()
+
+
+def display_data(rows=3):
+    try:
+        conn = duckdb.connect(DB_NAME)
+        cursor = conn.cursor()
+        sql_query = f"SELECT * FROM {DB_TABLE_FULL_NAME} ORDER BY Id DESC LIMIT {rows}"
+        data_rows = cursor.execute(sql_query).fetchall()
+
+        # Print the fetched rows
+        print(f"Displaying {rows} rows from the table : {DB_TABLE_FULL_NAME}")
+        for row in data_rows:
+            print(row)
+    except Exception as ex:
+        print(f"Error : {ex}")
+    finally:
+        # Close the connection
+        conn.close()
 
 
 def run_main_ingestion():
     try:
-        db.remove_database()
-        db.create_database()
+        db.run_main_db()
+        create_table()
         file_name = sys.argv[1]
         # file_name = "equalexperts_dataeng_exercise/my_uncommitted/votes_duplicate.jsonl"
         # file_name = "equalexperts_dataeng_exercise/my_uncommitted/votes_sample.jsonl"
-        print(file_name)
-        db.insert_data_into_database(file_name)
-        db.display_data(3)
+        print(f"File name :{file_name}")
+        insert_data_into_database(file_name)
+        display_data(3)
     except FileNotFoundError:
         print("Please download the dataset using 'poetry run exercise fetch-data'")
 
